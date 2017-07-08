fba2c4fc93e23bd00114992c195263d40e8a12a5
Thomas Heyenbrock <thomasheyenbrock@mymac.speedport.ip>
2017-07-08 21:41:52 +0200

    initial commit

---
+++ webpack/react/webpack.config.js
@@ -0,0 +1,46 @@
+var debug = process.env.NODE_ENV !== "production";
+var webpack = require('webpack');
+
+module.exports = {
+    context: __dirname,
+    devtool: debug ? "inline-sourcemap" : null,
+    entry: "./src/scripts.js",
+    output: {
+        path: __dirname + "/public/js",
+        filename: "scripts.min.js"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015', 'stage-0'],
+                    plugins: ['react-html-attrs', 'transform-decorators-legacy', 'transform-class-properties'],
+                }
+            },
+            {
+                test: /\.css$/,
+                loader: "style-loader!css-loader"
+            },
+            {
+                test: /.(png|woff(2)?|eot|ttf|svg)(\?[a-z0-9=\.]+)?$/,
+                loader: 'url-loader?limit=100000'
+            }
+        ]
+    },
+    plugins: debug ? [
+        new webpack.ProvidePlugin({'$': 'jquery','jQuery': 'jquery','window.jQuery': 'jquery'})
+    ] : [
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify('production')
+            }
+        }),
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin({mangle: false, sourcemap: false}),
+        new webpack.ProvidePlugin({'$': 'jquery','jQuery': 'jquery','window.jQuery': 'jquery'})
+    ]
+};
